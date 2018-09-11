62569ea019d94ba9286a46fd1d4dd4bc522ea371
AaEll <ellioa2@bu.edu>
2018-09-11 16:40:04 +0000

    update simulation

--- resources/MonteCarlo.py
+++ resources/MonteCarlo.py
@@ -1,30 +1,45 @@
-import random
+from random import random
+import scipy.stats
+
 
 def foo(X):
     print (X)
     print (X+1)
     return (X+1)
 
+
+
 class RandomVariable():
     
-    def __init__(self, dist = 'uniform on (0,1)',\
-                 inv_t= lambda x:x,\
+    def __init__(self, inv_t= lambda x:x, desc = 'no description',\
                  verbose = True):
-        self.distribution = dist
+        self.description = desc
         self.inverse_transform = inv_t
         if verbose:
-            print("testing random variable with distribution {},\n {}\n"\
-                  .format(self.distribution,self.sample()))
-                  
+            print("testing random variable with distribution {},\n {}\n".format(self.description,self.sample()))
                   
     def sample(self):
-        x = random.random()
+        x = random()
         return self.inverse_transform(x)
         
     def sample_repeated(self,n):
         while n>0:
             yield self.sample()
             n -= 1
+
+class Simulator():
+    def __init__(self, RV = RandomVariable(), cost_f = lambda x:x, desc = 'no description', verbose = 'True'):
+        self.cost_function = cost_f
+        self.description = desc
+        self.random_variable = RV
+        if verbose:
+            print("New simulator for {},\n with {} input".format(self.description, self.random_variable.description))
+    def sample(self):
+        return self.cost_function(self.random_variable.sample())
+    def sample_repeated(self,n):
+        for x in self.random_variable.sample_repeated(n):
+            yield self.cost_function(x)
+
             
 # inverse transforms for various common distributions
 def inverse_exponential():
@@ -33,6 +48,13 @@ def inverse_exponential():
 def inverse_continuous_power_law(x,xmin,alpha):
     assert (alpha != -1 and xmin > 0)
     return (xmin*((1-x)**(1/(alpha+1))))
+
 def inverse_discrete_power_law(x,xmin,alpha):
     assert(0)
     #TODO
+    
+# Useful statistics functions
+def Calculate_ConfIntv(sum1,sumsq,n,confidence = .95):
+    variance = (sumsq - sum1**2)/n
+    dev = scipy.stats.norm.ppf(confidence)*(variance**(.5))
+    print ("We obtained a {conf}% confidence interval of\n{mean} +- {deviation}".format(mean = sum1/n,conf=confidence,deviation=dev))
