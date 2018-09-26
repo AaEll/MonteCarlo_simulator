f3efdf7978cd20baf75c5a62d05a80e1780f9893
Aaron Elliot <aaronelliot27@gmail.com>
2018-09-26 18:49:53 +0000

    added MDP to MonteCarlo library, completed homework, and updated tradeoffs

--- resources/MonteCarlo.py
+++ resources/MonteCarlo.py
@@ -44,6 +44,64 @@ class Simulator():
     def sample_repeated(self,n):
         for x in self.random_variable.sample_repeated(n):
             yield self.cost_function(x)
+            
+class MDP():
+    termial_absorbing_state = "inf"
+    
+    def __init__(self, states, actions, transition_function, reward_function, initial_state_func, damping_constant, policy):
+        self.states = states
+        self.actions = actions
+        self.transition_function = transition_function
+        self.reward_function = reward_function
+        self.initial_state_func = initial_state_func
+        self.damping_constant = damping_constant
+        self.policy = policy
+        
+        self.current_state = self.initial_state_func()
+        self.current_reward = 0
+        self.timestep = 0
+
+        
+    def run_step(self):
+        action = self.policy(self.current_state,actions)
+        new_state = self.transition_function(self.current_state,action)
+        self.current_reward += self.reward_function(new_state,action)*self.damping_constant**(self.timestep)
+        self.timestep +=1
+        self.current_state = new_state
+    
+    def run_mdp(self):
+        self.current_state = self.initial_state_func()
+        self.current_reward = 0
+        self.timestep = 0
+        
+        while self.current_state != self.termial_absorbing_state:
+            self.run_step()
+        return self.current_reward
+    
+    def sample_repeated(self,n):
+        i = n
+        while (i>0):
+            i-=1
+            yield self.run_mdp()
+            
+    def check_boolean(self,boolean_func):
+        self.current_state = self.initial_state_func()
+        self.timestep = 0
+        
+        while self.current_state != self.termial_absorbing_state:
+            self.run_step()            
+            boolean = boolean_func(self)
+            if boolean is None:
+                pass
+            else:
+                return boolean
+        return False
+    def sample_boolean_repeated(self,boolean_func,n):
+        i = n
+        while (i>0):
+            i-=1
+            yield self.check_boolean(boolean_func)
+    
 
             
 # inverse transforms for various common distributions
@@ -63,3 +121,4 @@ def Calculate_ConfIntv(sum1,sumsq,n,confidence = .95):
     variance = (sumsq - sum1**2)/n
     dev = norm.ppf(confidence)*(variance**(.5))
     print ("We obtained a {conf}% confidence interval of\n{mean} +- {deviation}".format(mean = sum1/n,conf=confidence,deviation=dev))
+    
